b2a2c7e9987c1b5ddf5069414c108ef579fee4ca
iiHouSSaM <44523796+iiHouSSaM@users.noreply.github.com>
2018-11-17 11:38:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+onst Discord = require('discord.js');
 const client = new Discord.Client();
 const developers = ['343743154429755392','401406828820299777'];
 
@@ -16,17 +16,17 @@ client.on('message', message => {
 if (message.content.startsWith('2Wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
   client.user.setStatus("dnd")
-    message.channel.send(`${argresult} \` `)
+ message.channel.send(`Done. `)
 } else 
 if (message.content.startsWith('1Ls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
   client.user.setStatus("dnd")
-    message.channel.send(` ${argresult} \` `)
+ message.channel.send(`Done. `)
 } else 
 if (message.content.startsWith('3St')) {
   client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
   client.user.setStatus("dnd")
-   message.channel.send(`${argresult} \` `)
+   message.channel.send(`Done. `)
   
 }
   client.on('message', message => {//NouNouDz#6720
@@ -36,11 +36,10 @@ if (message.content.startsWith('3St')) {
   let channel = client.channels.get('511744532992229377');//NouNouDz#6720
 
   channel.join()//NouNouDz#6720
-  .then(connection => console.log(`** Done. **`))//NouNouDz#6720
+  .then(connection => console.log(`دخلت`))//NouNouDz#6720
   .catch(console.error);//NouNouDz#6720
   }
 });
           
 });
 client.login(process.env.BOT_TOKEN);
- 
