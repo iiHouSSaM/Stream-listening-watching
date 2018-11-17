022cdd8044318890caab5beb27da4cc8e9f2f310
iiHouSSaM <44523796+iiHouSSaM@users.noreply.github.com>
2018-11-17 11:50:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-onst Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const developers = ['343743154429755392','511744532992229377'];
 
@@ -16,17 +16,17 @@ client.on('message', message => {
 if (message.content.startsWith('2Wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
   client.user.setStatus("dnd")
- message.channel.send(`Done. `)
+    message.channel.send(`${argresult} \` `)
 } else 
 if (message.content.startsWith('1Ls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
   client.user.setStatus("dnd")
- message.channel.send(`Done. `)
+    message.channel.send(` ${argresult} \` `)
 } else 
 if (message.content.startsWith('3St')) {
   client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
   client.user.setStatus("dnd")
-   message.channel.send(`Done. `)
+   message.channel.send(`${argresult} \` `)
   
 }
   client.on('message', message => {//NouNouDz#6720
