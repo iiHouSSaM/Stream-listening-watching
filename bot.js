f22f8f432a572ed7a3c1122ebedf272e29968a9b
iiHouSSaM <44523796+iiHouSSaM@users.noreply.github.com>
2018-11-20 20:19:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['343743154429755392','514282475204247553'];
+const developers = ['343743154429755392','513961255199834113'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -13,7 +13,7 @@ client.user.setActivity(argresult, {type:'WATCHING'});
 } else 
 if (message.content.startsWith('Hls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
-  client.user.setStatus("dnd")
+  client.user.setStatus("idle")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Hst')) {
@@ -29,10 +29,9 @@ client.user.setActivity(argresult , {type:'PLAYING'});
 }
   client.on('message', message => {//iiHouSSaM#9494
         if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
-
   if (message.content === '963') {//iiHouSSaM#9494
-
-  let channel = client.channels.get('514282475204247553');//iiHouSSaM#9494
+   
+  let channel = client.channels.get('513961255199834113');//iiHouSSaM#9494
 
   channel.join()//iiHouSSaM#9494
   .then(connection => console.log(`????`))//iiHouSSaM#9494
