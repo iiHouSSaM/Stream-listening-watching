2c27415d2a8d8b54ebbe8bbb022909810cfd87c9
iiHouSSaM <44523796+iiHouSSaM@users.noreply.github.com>
2018-11-20 19:46:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,8 +29,8 @@ client.user.setActivity(argresult , {type:'PLAYING'});
 }
   client.on('message', message => {//iiHouSSaM#9494
         if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
-  if (message.content === '963') {//iiHouSSaM#9494
 
+  if (message.content === '963') {//iiHouSSaM#9494
    message.channel.send(` ** Done. :white_check_mark: ** `)
 
   let channel = client.channels.get('514282475204247553');//iiHouSSaM#9494
