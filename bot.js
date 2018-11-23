8e9cddf9c15529c220afa0cc9942e19fa1b8060a
iiHouSSaM <44523796+iiHouSSaM@users.noreply.github.com>
2018-11-23 11:19:59 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['343743154429755392','515057680826499074'];
+const developers = ['343743154429755392','515417209267290132'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -33,7 +33,7 @@ client.user.setActivity(argresult , {type:'PLAYING'});
 
 //if (!channel) return generale.join() .then(connection => message.channel.send(` \` ** Done.:white_check_mark:  ** `));  
 
-  let channel = client.channels.get('515057680826499074');//iiHouSSaM#9494
+  let channel = client.channels.get('515417209267290132');//iiHouSSaM#9494
 
 
   channel.join()//iiHouSSaM#9494
