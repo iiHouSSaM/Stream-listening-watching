05846389a49cc854d003811c20b4636ac78e93c6
iiHouSSaM <44523796+iiHouSSaM@users.noreply.github.com>
2018-11-24 12:34:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['343743154429755392','515786788078485515'];
+const developers = ['343743154429755392','515417209267290132'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -8,7 +8,7 @@ client.on('message', message => {
 
 if (message.content.startsWith('Hwt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
-  client.user.setStatus("idle")
+  client.user.setStatus("dnd")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Hls')) {
@@ -18,26 +18,29 @@ client.user.setActivity(argresult , {type:'LISTENING'});
 } else 
 if (message.content.startsWith('Hst')) {
   client.user.setGame(argresult, "https://www.twitch.tv/iitxbro");
-  client.user.setStatus("dnd")
+  client.user.setStatus("idle")
    message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Hpl')) {
 client.user.setActivity(argresult , {type:'PLAYING'});
   client.user.setStatus("idle")
     message.channel.send(` ** Done. :white_check_mark: ** `)
-  
+
 }
   client.on('message', message => {//iiHouSSaM#9494
         if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
-  if (message.content === '963') {//iiHouSSaM#9494 
+  if (message.content === '963') {//iiHouSSaM#9494
 
-  let channel = client.channels.get('515786788078485515');//iiHouSSaM#9494
+//if (!channel) return generale.join() .then(connection => message.channel.send(` \` ** Done.:white_check_mark:  ** `));  
 
-    
+  let channel = client.channels.get('515417209267290132');//iiHouSSaM#9494
+
+
   channel.join()//iiHouSSaM#9494
   .then(connection => console.log(`????`))//iiHouSSaM#9494
   .catch(console.error);//iiHouSSaM#9494
   }
 });
-
+          
+});
 client.login(process.env.BOT_TOKEN);
